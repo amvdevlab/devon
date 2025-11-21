c7d066ac0c593bab9f5f08b1f1f047ae13ad2d1d
Armando Valera <amvdevlab@gmail.com>
2025-11-21 17:24:08 -0500

    chore: add section 1. detectOS in ports.ts

--- src/core/ports.ts
+++ src/core/ports.ts
@@ -1,6 +1,24 @@
-// 1. Detect OS
-// 2. Choose command
-// 3. Execute
-// 4. Parse
-// 5. Normalize
-// 6. Return PortInfo[]
+import { PortInfo } from "./types";
+
+function detectOS() {
+  if (process.platform === "darwin") return "mac";
+  if (process.platform === "win32") return "windows";
+  if (process.platform === "linux") return "linux";
+  return null;
+}
+
+export async function getPorts() {
+  // 1. Detect OS
+  const os = detectOS();
+
+  if (os === null) {
+    console.log("Port scanning unsupported for this platform.");
+    return [];
+  }
+
+  // 2. Choose command
+  // 3. Execute
+  // 4. Parse
+  // 5. Normalize
+  // 6. Return PortInfo[]
+}
